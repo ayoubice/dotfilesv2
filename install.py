92b9da3860240a1006d5e4b5b153b07ba97da923
ayoubice <ayyoub.allali@northvolt.com>
2022-12-27 21:37:09 +0100

    add support of MacOS

--- install.py
+++ install.py
@@ -2,32 +2,67 @@
 
 import platform, os
 from pathlib import Path
+from shutil import which
 
 
 # tools  to be installed
-tools = ("vim", "tmux", "kitty", "zsh", "curl", "git", "ctag", "make", "fasd")
+tools = {
+	"Linux": ("vim", "tmux", "kitty", "zsh", "curl", "git", "ctags", "make", "fasd"),
+	"Darwin": ("vim", "tmux", "kitty", "zsh", "curl", "git", "ctags", "make", "fasd"),
+}
+
+
+# default brew bin
+brew = "/opt/homebrew/bin/brew" 
 
 source_base = os.getcwd()
 destination_base = Path.home()
 vundle_path = "{}/.vim/bundle/vundle".format(destination_base)
 zprezto_path= "{}/.zprezto".format(Path.home())
 
+# Prezto
+prezto_files = (
+    'zsh',
+    'zsh/zprezto',
+    'zsh/prezto-override/zshrc',
+    'zsh/prezto-override/zpreztorc',
+    'zsh/prezto/runcoms/zlogin',
+    'zsh/prezto/runcoms/zlogout',
+    'zsh/prezto/runcoms/zprofile',
+    'zsh/prezto/runcoms/zshenv',
+)
+
 def setup():
-    install(tools)
+    init_submodules()
+    install()
     config()
 
-def install(apps):
-    match platform.system():
-        case "Linux":
-            cmd_base='sudo apt-get install -y '
-            cmd= cmd_base + " ".join(apps)
-            os.system(cmd)
-            case _:
-                raise Exception("Unsupported platform")
+def install():
+	pltform = platform.system()
+
+	if pltform == "Linux":
+		cmd_base='sudo apt-get install -y '
+	elif pltform =="Darwin":
+		install_brew()
+		cmd_base="{} install ".format(brew)
+	else:
+		raise Exception("Unsupported platform")
+
+	cmd= cmd_base + " ".join(tools[pltform])
+	os.system(cmd)
+
+def install_brew():
+	if which(brew) is None:
+		os.system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+	
 
 def config():
-    # tmux
-    files = (
+    copy_config()
+    install_prezto()
+    install_vundler()
+
+def copy_config():
+	files = (
         # tmux
         'tmux/tmux.conf',
 
@@ -35,32 +70,50 @@ def config():
         'vimrc',
         'vim',
 
-        #zsh
-        'zsh/prezto-override/zshrc',
-        'zsh/prezto-override/zpreztorc',
-        '/runcoms/zlogin',
-
+        # kitty
     )
 
-    install_vundler()
+	for item in files:
+		copy(item)
 
 def copy(filepath, hidden = True):
-    file = os.path.basename(filepath)
+	file = os.path.basename(filepath)
+	file_dest = file
 
-    if hidden :
-        cmd="ln -nfs {src}/{file} {dest}/{file_dest}".format(
-            src = source_base, 
-            dest = destination_base,
-            file = filepath,
-            file_dest = ".{}".format(file),
-        )
+	if hidden:
+		file_dest = ".{}".format(file)
 
-        print(cmd)
-        # os.system(cmd)
+	os.system("ln -nfs {src}/{file} {dest}/{file_dest}".format(src = source_base, dest = destination_base, file = filepath, file_dest = file_dest))
+
+def unlink(file):
+	os.system("rm -f " + file)
 
 def install_vundler():
     install_cmd = "vim +VundleInstall +qall"
     cmd ="git clone https://github.com/gmarik/vundle.git {} && {}".format(vundle_path, install_cmd)
-    print(cmd)
+    os.system(cmd)
 
-setup()
+def remove_prezto():
+    for item in prezto_files:
+        unlink(item)
+
+def install_prezto():
+    if os.path.exists('zsh/zprezto') == False:
+        os.system('git clone --recursive https://github.com/sorin-ionescu/prezto.git "zsh/zprezto"')
+
+    for item in prezto_files:
+        copy(item)
+
+    folders = ('.zsh.before', '.zsh.after', '.zsh.prompts')
+    for folder in folders:
+        dest = "{}/{}".format(destination_base, folder)
+        if os.path.exists(dest) == False:
+            os.mkdir(dest)
+
+
+def config_kitty():
+    kitty_dir = "{dest}/.config/kitty".format(dest=destination_base)
+    os.system("rm -rf " + kitty_dir)
+    os.system("ln -nfs {src}/kitty {dest}/.config/".format(src = source_base, dest = destination_base))
+
+config_kitty()
--- vim/settings/vim-go.vim
+++ vim/settings/vim-go.vim
@@ -5,17 +5,23 @@ let g:go_template_use_pkg = 1
 let g:go_addtags_transform = 'snakecase'
 
 " key maps
-au FileType go nmap <leader> :GoVet<CR>
-nmap <buffer> gd <plug>(lsp-definition)
-    nmap <buffer> gs <plug>(lsp-document-symbol-search)
-        nmap <buffer> gS <plug>(lsp-workspace-symbol-search)
-            nmap <buffer> gr <plug>(lsp-references)
-                nmap <buffer> gi <plug>(lsp-implementation)
-                    nmap <buffer> gt <plug>(lsp-type-definition)
-                        nmap <buffer> <leader>rn <plug>(lsp-rename)
-                            nmap <buffer> [g <plug>(lsp-previous-diagnostic)
-                                nmap <buffer> ]g <plug>(lsp-next-diagnostic)
-                                    nmap <buffer> K <plug>(lsp-hover)
+autocmd FileType go nmap <leader>gr <Plug>(go-run)
+autocmd FileType go nmap <leader>gt <Plug>(go-test)
+autocmd FileType go nmap <Leader>gc <Plug>(go-coverage-toggle)
+autocmd FileType go nmap gd <plug>(go-def)
+autocmd FileType go nmap gr <plug>(go-referrers)
+autocmd FileType go nmap gi <plug>(go-implements)
+autocmd FileType go nmap gt <plug>(go-def-type)
+
+" nmap <buffer> gs <plug>(lsp-document-symbol-search)
+" nmap <buffer> gS <plug>(lsp-workspace-symbol-search)
+" nmap <buffer> <leader>rn <plug>(lsp-rename)
+" nmap <buffer> [g <plug>(lsp-previous-diagnostic)
+" nmap <buffer> ]g <plug>(lsp-next-diagnostic)
+" nmap <buffer> K <plug>(lsp-hover)
 
 " Use popup windows for Go Doc
 let g:go_doc_popup_window = 1
+
+" Reuse buffer when navigating to definitions
+let g:go_def_reuse_buffer = 1
